2bd4b15af3a4b0ae0f1310c029b00769afb5044f
Ex Balance <exbalance2@gmail.com>
2025-12-13 01:07:45 +0700

    Cleanup: Remove database debug logging

--- lib/db.ts
+++ lib/db.ts
@@ -17,16 +17,7 @@ const pool = mysql.createPool({
     queueLimit: 0,
 });
 
-if (dbUrl) {
-    console.log('--- DB Connection Debug ---');
-    console.log('Host:', dbUrl.hostname);
-    console.log('User:', decodeURIComponent(dbUrl.username));
-    const pass = decodeURIComponent(dbUrl.password);
-    console.log('Pass Length:', pass.length);
-    console.log('Pass First 3:', pass.substring(0, 3));
-    console.log('Pass Last 3:', pass.substring(pass.length - 3));
-    console.log('---------------------------');
-}
+
 
 // ฟังก์ชันสำหรับ query ข้อมูล
 export async function query<T = any>(sql: string, params: any[] = []): Promise<T> {
